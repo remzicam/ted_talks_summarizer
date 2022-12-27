1998356b958faa93390cbec0bbf2349f68544690
remzicam <65707131+remzicam@users.noreply.github.com>
2022-12-27 18:09:36 +0100

    Add files via upload

--- gradio_app/app.py
+++ gradio_app/app.py
@@ -2,11 +2,23 @@
 
 from re import sub
 
-from gradio import Interface, Series, Textbox
+from gradio import Interface, Textbox
 from requests import get
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+
+repo_id = "pszemraj/led-base-book-summary"
+
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    repo_id,
+    low_cpu_mem_usage=True,
+)
+
+tokenizer = AutoTokenizer.from_pretrained(repo_id)
+
+summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
 
 
-def clean_text(text):
+def clean_text(text: str) -> str:
     """Cleans subtitle text of ted talks.
 
     Args:
@@ -26,14 +38,14 @@ def clean_text(text):
     return cleaned_text
 
 
-def ted_talk_transcriber(link):
+def ted_talk_transcriber(link: str) -> str:
     """Creates transcription of ted talks from url.
 
     Args:
         link (str): url link of ted talks
 
     Returns:
-        cleaned_transcript (str): transcription of the ted talk
+        raw_text (str): raw transcription of the ted talk
     """
     # request link of the talk
     page = get(link)
@@ -42,29 +54,55 @@ def ted_talk_transcriber(link):
     raw_text = get(
         f"https://hls.ted.com/project_masters/{talk_id}/subtitles/en/full.vtt"
     ).text
+    return raw_text
+
+
+def text_summarizer(text: str) -> str:
+    """Summarizes given text.
+
+    Args:
+        text (str): ted talks transcription
+
+    Returns:
+        str: summary
+    """
+    result = summarizer(
+        text,
+        min_length=8,
+        max_length=256,
+        no_repeat_ngram_size=3,
+        encoder_no_repeat_ngram_size=3,
+        repetition_penalty=3.5,
+        num_beams=4,
+        do_sample=False,
+        early_stopping=True,
+    )
+    return result[0]["summary_text"]
+
+
+def main(link: str) -> str:
+    """Summarizes ted talks given link.
+
+    Args:
+        link (str): url link of ted talks
+
+    Returns:
+        str: summary
+    """
+    raw_text = ted_talk_transcriber(link)
     cleaned_transcript = clean_text(raw_text)
-    return cleaned_transcript
+    return text_summarizer(cleaned_transcript)
 
-
-transcriber = Interface(
-    ted_talk_transcriber,
-    "text",
-    "text",
-)
-
-summarizer = Interface.load(
-    "huggingface/pszemraj/long-t5-tglobal-base-16384-book-summary"
-)
 
 logo = "<center><img src='file/TED.png' width=180px></center>"
 
-Series(
-    transcriber,
-    summarizer,
+Interface(
+    main,
     inputs=Textbox(label="Type the TED Talks link"),
     examples=[
-        "https://www.ted.com/talks/jen_gunter_the_truth_about_yeast_in_your_body"
-    ],
+    "https://www.ted.com/talks/jen_gunter_the_truth_about_yeast_in_your_body"
+             ],
+    outputs=Textbox(label="Summary"),
     allow_flagging="never",
     description=logo,
 ).launch()
